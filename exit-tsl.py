27e16313c4be328a8b01e25d9856245c76cd0a4c
David <88436424+DavidGCalles@users.noreply.github.com>
2022-01-19 17:30:04 +0100

    Vamos implementando poco a poco. Hemos tenido que hacer un rollback y... bueno. Ya está. Pasó. Fixes #61

--- exit-tsl.py
+++ exit-tsl.py
@@ -19,6 +19,7 @@ class TSLexit(Worker):
 	def __init__(self, user):
 		super().__init__(user, "TSL")
 		self.pingInterval = timedelta(seconds=10)
+		self.logger.info("Start")
 	def isUnattended(self, lastCheck, thresold):
 		if lastCheck != None:
 			if lastCheck <= datetime.now()-thresold:
@@ -55,19 +56,19 @@ class TSLexit(Worker):
 	def startWork(self):
 		self.twm = ThreadedWebsocketManager(api_key=self.API[0], api_secret=self.API[1])
 		self.twm.start()
-		print(f"{datetime.now()}- Obteniendo trades abiertos")
+		self.logger.debug(f"Getting Open Trades")
 		self.trades = self.db.getOpenTrades()
 		self.streams = {}
 		self.lastCheck = datetime.now()
 		for trade in self.trades:
 			self.streams[trade["symbol"]] = {}
 			self.streams[trade["symbol"]]["trade"] = trade
-			print(f"{datetime.now()}- Inicializando Socket: {trade['symbol']}")
+			self.logger.info(f"Starting Socket: {trade['symbol']}")
 			self.streams[trade["symbol"]]["db"] = DB()
 			self.streams[trade["symbol"]]["stream"] = self.twm.start_symbol_ticker_socket(callback=self.handle_socket_message, symbol=trade["symbol"])
-		print(f"{datetime.now()}- Dando tiempo a los sockets a establecerse")
+		self.logger.debug(f"Giving time to sockets to establish")
 		sleep(20)
-		print(f"{datetime.now()}- Comenzando monitoreo de desatendidos.")
+		self.logger.info(f"Starting unattended monitor")
 		while True: 
 			if self.lastCheck <= datetime.now()-timedelta(seconds=30): ## Aqui hay que usar self.interval
 				#print(f"Tick: {datetime.now()}")
@@ -75,19 +76,19 @@ class TSLexit(Worker):
 				self.lastCheck = datetime.now()
 				for trade in self.trades:
 					if self.isUnattended(trade["lastCheck"], timedelta(seconds=30)): ##Thresold, ira a configuracion
-						print(f"{datetime.now()}- Desatendido: {trade['symbol']} | {trade['lastCheck']}")
+						self.logger.warning(f"Unattended: {trade['symbol']}", extra={"symbol": trade['symbol'], "lastCheck": trade['lastCheck']})
 						try:
 							self.twm.stop_socket(self.streams[trade["symbol"]]["stream"])
 						except:
-							print(f"{datetime.now()}- Error cerrando el socket")
+							self.logger.error(f"Error closing socket")
 						if trade["softLimit"] == None:
 							self.setLimits(trade, trade["price"])
 						self.streams[trade["symbol"]] = {}
 						self.streams[trade["symbol"]]["trade"] = trade
 						self.streams[trade["symbol"]]["db"] = DB()
-						print(f"{datetime.now()}- Reiniciando socket: {trade['symbol']}")
+						self.logger.info(f"Restarting socket: {trade['symbol']}")
 						self.streams[trade["symbol"]]["stream"] = self.twm.start_symbol_ticker_socket(callback=self.handle_socket_message, symbol=trade["symbol"])
-						print(f"{datetime.now()}- Socket Reiniciado")
+						self.logger.debug(f"Restarted socket: {trade['symbol']}")
 
 if __name__ == "__main__":
 	##Instantiate Class
